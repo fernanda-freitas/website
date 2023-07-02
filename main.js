c2deeca89b7a47cec273a743cb463aeee41910e9
Fernanda Freitas <idea.fernanda@gmail.com>
2023-07-02 16:50:42 +0100

    Add projects and highlight section

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
 const title1 = document.querySelector('[data-id="title1"]');
 const title2 = document.querySelector('[data-id="title2"]')
 const title3 = document.querySelector('[data-id="title3"]')
+const projectsHighlight = document.querySelector('[data-id="highlight-projects__items"]')
+const projectsHighlightSmall = document.querySelector('[data-id="highlight-info__items-small"]')
 const projects = document.querySelector('[data-id="project-section"]')
 const ctaButton = document.querySelector('.btn-cta')
 
 const heroScrollTimeline = new ScrollTimeline({
-  scrollOffsets: [CSS.vh(0), window.visualViewport.height]
+  scrollOffsets: [CSS.vh(0), CSS.vh(100)]
 });
 
 const stackScrollTimeline = new ScrollTimeline({
@@ -17,6 +19,14 @@ const stackScrollTimeline = new ScrollTimeline({
     { target: title3, edge: "start", threshold: "1" }
   ]
 });
+
+const projectsHighlightTimeline = new ScrollTimeline({
+  scrollOffsets: [
+    { target: projectsHighlight, edge: "end", threshold: "0" },
+    { target: projectsHighlight, edge: "start", threshold: "0" }
+  ]
+});
+
 
 title1.animate(
   { transform: ['translateX(0px)', 'translateX(1200px)'] },
@@ -29,9 +39,19 @@ title2.animate(
 );
 
 title3.animate(
-  { transform: ['translateY(180px)', 'translateY(-100px)'] },
+  { transform: ['translateY(200px)', 'translateY(-80px)'] },
   { duration: 1, timeline: stackScrollTimeline, easing: 'ease-in-out' }
 );
+
+projectsHighlight.animate(
+  { transform: ['translate3d(900px, 0px, 0px)', 'translate3d(-157.253px, 0px, 0px)'] },
+  { duration: 1, timeline: projectsHighlightTimeline, easing: 'ease-in-out' }
+)
+
+projectsHighlightSmall.animate(
+  { transform: ['translate3d(-500px, 0px, 0px)', 'translate3d(0px, 0px, 0px)'] },
+  { duration: 1, timeline: projectsHighlightTimeline, easing: 'ease-in-out' }
+)
 
 projects.addEventListener('wheel', (e) => {
   const scrollWidth = projects.scrollWidth
