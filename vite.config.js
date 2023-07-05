3dccba4d9f5ef2327df5a5672c5bba794817eaa4
Fernanda Freitas <idea.fernanda@gmail.com>
2023-07-05 16:37:02 +0100

    Vite folder structure for routing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+// const environmentType = process.env.NODE_ENV
+// const base = environmentType === 'production' ? '/website' : '/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // base: base,
     base: '/website',
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            construir: resolve(__dirname, 'construir.html'),
+          },
+        },
+      },
 })
